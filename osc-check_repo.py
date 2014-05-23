5ffce7895e331ef3d231b326782ba8d15071cc5f
Alberto Planas <aplanas@suse.com>
2014-05-23 09:34:13 +0200

    Move Graph into osclib.

--- osc-check_repo.py
+++ osc-check_repo.py
@@ -35,6 +35,7 @@ from osc.core import Request
 # Expand sys.path to search modules inside the pluging directory
 _plugin_dir = os.path.expanduser('~/.osc-plugins')
 sys.path.append(_plugin_dir)
+from osclib.graph import Graph
 from osclib.stagingapi import StagingAPI
 
 #
@@ -49,7 +50,6 @@ global shelve
 global tempfile
 global wraps
 
-global Graph
 global Package_
 
 global memoize
@@ -58,126 +58,6 @@ global build
 global last_build_success
 global builddepinfo
 global jobhistory
-
-
-class Graph(dict):
-    """Graph object. Inspired in NetworkX data model."""
-
-    def __init__(self):
-        """Initialize an empty graph."""
-        #  The nodes are stored in the Graph dict itself, but the
-        #  adjacent list is stored as an attribute.
-        self.adj = {}
-
-    def add_node(self, name, value):
-        """Add a node in the graph."""
-        self[name] = value
-        if name not in self.adj:
-            self.adj[name] = set()
-
-    def add_nodes_from(self, nodes_and_values):
-        """Add multiple nodes"""
-        for node, value in nodes_and_values:
-            self.add_node(node, value)
-
-    def add_edge(self, u, v, directed=True):
-        """Add the edge u -> v, an v -> u if not directed."""
-        self.adj[u].add(v)
-        if not directed:
-            self.adj[v].add(u)
-
-    def add_edges_from(self, edges, directed=True):
-        """Add the edges from an iterator."""
-        for u, v in edges:
-            self.add_edge(u, v, directed)
-
-    def remove_edge(self, u, v, directed=True):
-        """Remove the edge u -> v, an v -> u if not directed."""
-        try:
-            self.adj[u].remove(v)
-        except KeyError:
-            pass
-        if not directed:
-            try:
-                self.adj[v].remove(u)
-            except KeyError:
-                pass
-
-    def remove_edges_from(self, edges, directed=True):
-        """Remove the edges from an iterator."""
-        for u, v in edges:
-            self.remove_edge(u, v, directed)
-
-    def edges(self, v):
-        """Get the adjancent list for a vertex."""
-        return sorted(self.adj[v]) if v in self else ()
-
-    def edges_to(self, v):
-        """Get the all the vertex that point to v."""
-        return sorted(u for u in self.adj if v in self.adj[u])
-
-    def cycles(self):
-        """Detect cycles using Tarjan algorithm."""
-        index = [0]
-        path = []
-        cycles = []
-
-        v_index = {}
-        v_lowlink = {}
-
-        def scc(node, v):
-            v_index[v], v_lowlink[v] = index[0], index[0]
-            index[0] += 1
-            path.append(node)
-
-            for succ in self.adj.get(node, []):
-                w = self[succ]
-                if w not in v_index:
-                    scc(succ, w)
-                    v_lowlink[v] = min(v_lowlink[v], v_lowlink[w])
-                elif succ in path:
-                    v_lowlink[v] = min(v_lowlink[v], v_index[w])
-
-            if v_index[v] == v_lowlink[v]:
-                i = path.index(node)
-                path[:], cycle = path[:i], frozenset(path[i:])
-                if len(cycle) > 1:
-                    cycles.append(cycle)
-
-        for node in sorted(self):
-            v = self[node]
-            if not getattr(v, 'index', 0):
-                scc(node, v)
-        return frozenset(cycles)
-
-    # XXX - Deprecated - Remove in a future release
-    def cycles_fragments(self):
-        """Detect partial cycles using DFS."""
-        cycles = set()
-        visited = set()
-        path = []
-
-        def dfs(node):
-            if node in visited:
-                return
-
-            visited.add(node)
-            path.append(node)
-            for succ in self.adj.get(node, []):
-                try:
-                    i = path.index(succ)
-                except ValueError:
-                    i = None
-                if i is not None:
-                    cycle = path[i:]
-                    cycles.add(frozenset(cycle))
-                else:
-                    dfs(succ)
-            path.pop()
-
-        for node in sorted(self):
-            dfs(node)
-        return frozenset(cycles)
 
 
 class Package_(object):
@@ -388,7 +268,7 @@ def get_project_repos(apiurl, src_project, tgt_project, src_package, rev):
 
 def old_md5(apiurl, src_project, tgt_project, src_package, rev):
     """Recollect old MD5 for a package."""
-    # XXX TODO - instead of fixing the limit, use endtime to makes
+    # XXX TODO - instead of fixing the limit, use endtime to make
     # sure that we have the correct time frame.
     limit = 20
     query = {
---
+++ osclib/graph.py
@@ -0,0 +1,105 @@
+# Copyright (C) 2014 SUSE Linux Products GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+class Graph(dict):
+    """Graph object. Inspired in NetworkX data model."""
+
+    def __init__(self):
+        """Initialize an empty graph."""
+        #  The nodes are stored in the Graph dict itself, but the
+        #  adjacent list is stored as an attribute.
+        self.adj = {}
+
+    def add_node(self, name, value):
+        """Add a node in the graph."""
+        self[name] = value
+        if name not in self.adj:
+            self.adj[name] = set()
+
+    def add_nodes_from(self, nodes_and_values):
+        """Add multiple nodes"""
+        for node, value in nodes_and_values:
+            self.add_node(node, value)
+
+    def add_edge(self, u, v, directed=True):
+        """Add the edge u -> v, an v -> u if not directed."""
+        self.adj[u].add(v)
+        if not directed:
+            self.adj[v].add(u)
+
+    def add_edges_from(self, edges, directed=True):
+        """Add the edges from an iterator."""
+        for u, v in edges:
+            self.add_edge(u, v, directed)
+
+    def remove_edge(self, u, v, directed=True):
+        """Remove the edge u -> v, an v -> u if not directed."""
+        try:
+            self.adj[u].remove(v)
+        except KeyError:
+            pass
+        if not directed:
+            try:
+                self.adj[v].remove(u)
+            except KeyError:
+                pass
+
+    def remove_edges_from(self, edges, directed=True):
+        """Remove the edges from an iterator."""
+        for u, v in edges:
+            self.remove_edge(u, v, directed)
+
+    def edges(self, v):
+        """Get the adjancent list for a vertex."""
+        return sorted(self.adj[v]) if v in self else ()
+
+    def edges_to(self, v):
+        """Get the all the vertex that point to v."""
+        return sorted(u for u in self.adj if v in self.adj[u])
+
+    def cycles(self):
+        """Detect cycles using Tarjan algorithm."""
+        index = [0]
+        path = []
+        cycles = []
+
+        v_index = {}
+        v_lowlink = {}
+
+        def scc(node, v):
+            v_index[v], v_lowlink[v] = index[0], index[0]
+            index[0] += 1
+            path.append(node)
+
+            for succ in self.adj.get(node, []):
+                w = self[succ]
+                if w not in v_index:
+                    scc(succ, w)
+                    v_lowlink[v] = min(v_lowlink[v], v_lowlink[w])
+                elif succ in path:
+                    v_lowlink[v] = min(v_lowlink[v], v_index[w])
+
+            if v_index[v] == v_lowlink[v]:
+                i = path.index(node)
+                path[:], cycle = path[:i], frozenset(path[i:])
+                if len(cycle) > 1:
+                    cycles.append(cycle)
+
+        for node in sorted(self):
+            v = self[node]
+            if not getattr(v, 'index', 0):
+                scc(node, v)
+        return frozenset(cycles)
