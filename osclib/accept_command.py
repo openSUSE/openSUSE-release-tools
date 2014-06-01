454828d2528477fb1fb4c743adaf71cc32368a03
Stephan Kulow <coolo@suse.de>
2014-06-01 19:51:18 +0200

    accept other new requests after accepting staging prjs

--- osclib/accept_command.py
+++ osclib/accept_command.py
@@ -1,21 +1,37 @@
 from osc.core import change_request_state
 from osc.core import get_request
+from osc.core import http_GET
 
 from osclib.comments import CommentAPI
 
+from xml.etree import cElementTree as ET
 
 class AcceptCommand(object):
     def __init__(self, api):
         self.api = api
         self.comment = CommentAPI(self.api.apiurl)
 
+    def find_new_requests(self, project):
+        requests = []
+
+        query = "match=state/@name='new'+and+(action/target/@project='{}'+and+action/@type='submit')".format(project)
+        url = self.api.makeurl(['search', 'request'], query)
+
+        f = http_GET(url)
+        root = ET.parse(f).getroot()
+        
+        ids=[]
+        for rq in root.findall('request'):
+            ids.append(int(rq.get('id')))
+        return ids
+            
     def perform(self, project):
         """
         Accept the staging LETTER for review and submit to factory
         Then disable the build to disabled
         :param project: staging project we are working with
         """
-
+                
         status = self.api.check_project_status(project)
 
         if not status:
@@ -45,3 +61,10 @@ class AcceptCommand(object):
         if self.api.project_exists(project + ":DVD"):
             self.api.build_switch_prj(project + ":DVD", 'disable')
 
+        return True
+
+    def accept_other_new(self):
+        for req in self.find_new_requests('openSUSE:Factory'):
+            change_request_state(self.api.apiurl, str(req), 'accepted', message='Accept to factory')
+
+        return True
