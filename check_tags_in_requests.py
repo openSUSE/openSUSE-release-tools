9e690d77f161490ed820d86bd7110eae3a023dab
Antonio Larrosa <antonio@solaria>
2015-03-25 01:58:19 +0100

    Fix year in copyright note

--- check_tags_in_requests.py
+++ check_tags_in_requests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2014 SUSE Linux GmbH
+# Copyright (c) 2015 SUSE Linux GmbH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
