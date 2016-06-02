f2db5cdb5e77127d10dab141031e1a0b67420539
Ludwig Nussel <ludwig.nussel@suse.de>
2016-06-02 13:22:31 +0200

    Use implicit whitespace split

--- osclib/stagingapi.py
+++ osclib/stagingapi.py
@@ -53,8 +53,8 @@ class StagingAPI(object):
         # Store some prefix / data used in the code.
         self.cstaging = conf.config[project]['staging']
         self.cstaging_group = conf.config[project]['staging-group']
-        self.cstaging_archs = conf.config[project]['staging-archs'].split(' ')
-        self.cstaging_dvd_archs = conf.config[project]['staging-dvd-archs'].split(' ')
+        self.cstaging_archs = conf.config[project]['staging-archs'].split()
+        self.cstaging_dvd_archs = conf.config[project]['staging-dvd-archs'].split()
         self.cstaging_nocleanup = conf.config[project]['nocleanup-packages'].split()
         self.crings = conf.config[project]['rings']
         self.cnonfree = conf.config[project]['nonfree']
