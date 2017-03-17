5927b2b283ce60fd4b5d47c9624c98bcf670f37f
Jimmy Berry <jberry@suse.com>
2017-03-17 14:41:11 -0500

    unignore: utilize RequestFinder.

--- osclib/unignore_command.py
+++ osclib/unignore_command.py
@@ -2,13 +2,14 @@ import dateutil.parser
 from datetime import datetime
 
 from osc.core import get_request
+from osclib.request_finder import RequestFinder
 
 
 class UnignoreCommand(object):
     def __init__(self, api):
         self.api = api
 
-    def perform(self, request_ids, cleanup=False):
+    def perform(self, requests, cleanup=False):
         """
         Unignore a request by removing from ignore list.
         """
@@ -16,11 +17,10 @@ class UnignoreCommand(object):
         requests_ignored = self.api.get_ignored_requests()
         length = len(requests_ignored)
 
-        if len(request_ids) == 1 and request_ids[0] == 'all':
+        if len(requests) == 1 and requests[0] == 'all':
             requests_ignored = {}
         else:
-            for request_id in request_ids:
-                request_id = int(request_id)
+            for request_id in RequestFinder.find_sr(requests, self.api):
                 if request_id in requests_ignored:
                     print('Removing {}'.format(request_id))
                     del requests_ignored[request_id]
