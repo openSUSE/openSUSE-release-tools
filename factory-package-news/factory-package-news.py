b94b69703e4b37ea5fc6b1c1a84de9e30c601e59
Jimmy Berry <jberry@suse.com>
2017-03-23 18:06:17 -0500

    package-news: include s390x in search path.

--- factory-package-news/factory-package-news.py
+++ factory-package-news/factory-package-news.py
@@ -119,7 +119,7 @@ class ChangeLogger(cmdln.Cmdln):
                 if not iso.is_open() or fd is None:
                     raise Exception("Could not open %s as an ISO-9660 image." %  arg)
 
-                for path in ['/suse/x86_64/', '/suse/noarch']:
+                for path in ['/suse/x86_64', '/suse/noarch', '/suse/s390x']:
                     file_stats = iso.readdir(path)
                     if file_stats is None:
                         continue
