7c6f67979a4b29d7d81eae3bff6da668217df3da
Jimmy Berry <jberry@suse.com>
2017-05-11 23:31:27 -0500

    core: extract maintainers_get(), owner_fallback() from MaintenanceChecker.
    Allows the code to be properly shared between checkrepo and
    check_maintenance_incidents as a todo suggests. Given that the majority of
    similar cases for code sharing are extension of osc.core it seems to make
    sense to place them in osclib.core.

--- osclib/core.py
+++ osclib/core.py
@@ -1,10 +1,32 @@
 from xml.etree import cElementTree as ET
 
+import osc.core
 from osc.core import http_GET
 from osc.core import makeurl
 from osc.core import show_project_meta
 
 from osclib.memoize import memoize
+
+
+@memoize(session=True)
+def owner_fallback(apiurl, project, package):
+    root = osc.core.owner(apiurl, package, project=project)
+    owner = root.find('owner')
+    if not owner or owner.get('project') == project:
+        # Fallback to global (ex Factory) maintainer.
+        root = osc.core.owner(apiurl, package)
+    return root
+
+def maintainers_get(apiurl, project, package):
+    """Get the maintainer of the package involved in the package."""
+    root = owner_fallback(apiurl, project, package)
+    maintainers = [p.get('name') for p in root.findall('.//person') if p.get('role') == 'maintainer']
+    if not maintainers:
+        for group in [p.get('name') for p in root.findall('.//group') if p.get('role') == 'maintainer']:
+            url = makeurl(apiurl, ('group', group))
+            root = ET.parse(http_GET(url)).getroot()
+            maintainers = maintainers + [p.get('userid') for p in root.findall('./person/person')]
+    return maintainers
 
 @memoize(session=True)
 def package_list(apiurl, project):
