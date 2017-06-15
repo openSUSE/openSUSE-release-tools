cd937e1f6141bb7c9beaac5d4bf5648dfae435e8
Jimmy Berry <jberry@suse.com>
2017-06-15 17:07:19 -0500

    openqa-comment: add headings to avoid paragraph formatting mess.

--- openqa-comments.py
+++ openqa-comments.py
@@ -183,6 +183,10 @@ class OpenQAReport(object):
         report_openQA, some_openqa_fail = self._report_openQA(info)
 
         if report_broken_packages or some_openqa_fail:
+            if report_broken_packages:
+                report_broken_packages = 'Broken:\n\n' + report_broken_packages
+            if report_openQA:
+                report_openQA = 'openQA:\n\n' + report_openQA
             report = '\n\n'.join((report_broken_packages, report_openQA))
             report = report.strip()
             if report:
