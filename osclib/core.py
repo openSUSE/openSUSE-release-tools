c946aac3c351e49a436e244d13a1984a99799d04
Jimmy Berry <jberry@suse.com>
2017-06-14 23:50:57 -0500

    core: provide target_archs().

--- osclib/core.py
+++ osclib/core.py
@@ -2,6 +2,7 @@ from xml.etree import cElementTree as ET
 
 from osc.core import http_GET
 from osc.core import makeurl
+from osc.core import show_project_meta
 
 from osclib.memoize import memoize
 
@@ -15,3 +16,12 @@ def package_list(apiurl, project):
         packages.append(package.get('name'))
 
     return sorted(packages)
+
+@memoize(session=True)
+def target_archs(apiurl, project):
+    meta = show_project_meta(apiurl, project)
+    meta = ET.fromstring(''.join(meta))
+    archs = []
+    for arch in meta.findall('repository[@name="standard"]/arch'):
+        archs.append(arch.text)
+    return archs
