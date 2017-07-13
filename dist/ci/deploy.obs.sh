7df5bf27c740e1ecaf23a2ef9f90e6ad6b9f1829
Jimmy Berry <jberry@suse.com>
2017-07-13 18:21:56 -0500

    travis: add deployment to OBS package on master branch commit.

---
+++ dist/ci/deploy.obs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat << eom > ~/.oscrc
+[general]
+[https://api.opensuse.org]
+user = $OBS_USER
+pass = $OBS_PASS
+eom
+
+osc checkout "$OBS_PACKAGE"
+cd "$OBS_PACKAGE"
+
+osc service disabledrun
+echo >> _servicedata
+osc addremove
+osc commit -m "$(grep -oP 'version: \K.*' *.obsinfo)"
