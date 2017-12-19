openSUSE/openSUSE-release-tools

commit 3e5f3b1377f18b9a32474ac210490ed370dafcf9

Add support for non-x86_64 products (Leap 15.0/aarch64)

pkglistgen.py
@@ -22,6 +22,7 @@
 
 # TODO: implement equivalent of namespace namespace:language(de) @SYSTEM
 # TODO: solve all devel packages to include
+from __future__ import print_function
 
 from lxml import etree as ET
 from collections import namedtuple
@@ -69,7 +70,7 @@ class Group(object):
         self.solved = False
         self.not_found = dict()
         self.unresolvable = dict()
-        for a in self.architectures:
+        for a in ARCHITECTURES:
             self.packages[a] = []
             self.unresolvable[a] = dict()
 
@@ -436,7 +437,7 @@ class PkgListGen(ToolBase.ToolBase):
                 if status == self.default_support_status:
                     continue
                 for group in self.packages[name][status]:
-                    print name, status
+                    print(name, status)
 
     def _load_supportstatus(self):
         # XXX
@@ -670,7 +671,7 @@ class CommandLineInterface(ToolBase.CommandLineInterface):
         self.tool.load_all_groups()
 
         for name in sorted(self.tool.groups.keys()):
-            print name
+            print(name)
 
     # to be called only once to bootstrap
     def do_dump_supportstatus(self, subcmd, opts):

pkglistgen.sh
@@ -10,6 +10,8 @@ self=$(readlink -e $(type -p "$0"))
 : ${project:=openSUSE:Factory}
 : ${api:=api.opensuse.org}
 : ${repos:=$project/standard}
+: ${productrepo:=standard}
+: ${arch:=x86_64}
 
 groups="000package-groups"
 product="000product"
@@ -50,7 +52,7 @@ if ! osc api "/source/$project/" | grep -q "$product"  ; then
 	echo "$product undeleted, skip dvd until next cycle"
 	exit 0
 elif [ -z "$FORCE" ]; then
-	bs_status=`osc api "/build/$project/_result?package=$product&repository=standard"`
+	bs_status=`osc api "/build/$project/_result?package=$product&repository=$productrepo"`
 	if echo "${bs_status}" | grep -q 'building\|dirty'; then
 		echo "$project build in progress, skipping."
 		exit 0
@@ -94,10 +96,10 @@ rm -f supportstatus.txt groups.yml package-groups.changes
 for i in *.spec.in; do
   mv -v $i "${i%.in}"
 done
-if ! ${self%.sh}.py -i "$cachedir/$groups" -r $repos -o . -a x86_64 update; then
+if ! ${self%.sh}.py -i "$cachedir/$groups" -r $repos -o . -a $arch update; then
 	echo "no change in packages"
 fi
-${self%.sh}.py -i "$cachedir/$groups" -r $repos -o . -a x86_64 solve "${solveargs[@]}"
+${self%.sh}.py -i "$cachedir/$groups" -r $repos -o . -a $arch solve "${solveargs[@]}"
 for i in $delete_products; do
 	rm -vf -- "$i"
 done

script/osrt-pkglistgen-openSUSE:Leap:15.0
@@ -1,4 +1,3 @@
 #!/bin/bash
 export project=openSUSE:Leap:15.0
-export repos=$project/standard
 osrt-pkglistgen

script/osrt-pkglistgen-openSUSE:Leap:15.0:Ports-aarch64.sh
@@ -1,0 +1,7 @@
+#!/bin/bash
+self=$(readlink $(type -p "$0"))
+export project=openSUSE:Leap:15.0:Ports
+export repos=$project/ports
+export arch=aarch64
+export productrepo=ports
+osrt-pkglistgen

commit 31da422cdac0d935244a408b5066019cdc326e87

Fix travis failure introduced with 3e5f3b137

pkglistgen.py
@@ -798,10 +798,10 @@ class CommandLineInterface(ToolBase.CommandLineInterface):
                 del pool
 
         for reponame in sorted(set(drops.values())):
-            print "<!-- %s -->" % reponame
+            print("<!-- %s -->" % reponame)
             for p in sorted(drops):
                 if drops[p] != reponame: continue
-                print "  <obsoletepackage>%s</obsoletepackage>" % p
+                print("  <obsoletepackage>%s</obsoletepackage>" % p)
 
     @cmdln.option('--overwrite', action='store_true', help='overwrite if output file exists')
     def do_dump_solv(self, subcmd, opts, baseurl):