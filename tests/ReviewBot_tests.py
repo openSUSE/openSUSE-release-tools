9c44f78cdf72bbb0f3c2f47e198a7abd890c9b09
Jimmy Berry <jberry@suse.com>
2018-01-03 17:52:14 -0600

    tests/ReviewBot_tests: add test_workflow() complex test.

--- tests/ReviewBot_tests.py
+++ tests/ReviewBot_tests.py
@@ -43,6 +43,67 @@ class TestReviewBotComment(OBSLocalTestCase):
         comment, _ = self.comments_filtered(self.bot)
         self.assertTrue(comment['comment'].endswith('something extra'))
 
+    def test_workflow(self):
+        comment_count = len(self.api.get_comments(project_name=PROJECT))
+        self.assertFalse(self.comments_filtered(self.bot)[0])
+
+        # Initial comment.
+        info = {'state': 'seen', 'result': 'failed'}
+        info_extra = {'build': '1'}
+        info_merged = info.copy()
+        info_merged.update(info_extra)
+        self.review_bot.comment_write(state='seen', result='failed', identical=True,
+                                      info_extra=info_extra, info_extra_identical=False,
+                                      project=PROJECT, message=COMMENT)
+        comment, info_parsed = self.comments_filtered(self.bot)
+        self.assertTrue(comment['comment'].endswith(COMMENT))
+        self.assertEqual(info_parsed, info_merged)
+
+        # Only build change (expect no change).
+        info_extra = {'build': '2'}
+        self.review_bot.comment_write(state='seen', result='failed', identical=True,
+                                      info_extra=info_extra, info_extra_identical=False,
+                                      project=PROJECT, message=COMMENT)
+        comment, info_parsed = self.comments_filtered(self.bot)
+        self.assertTrue(comment['comment'].endswith(COMMENT))
+        self.assertEqual(info_parsed, info_merged)
+
+        # Build and comment (except comment replacement).
+        info_extra = {'build': '3'}
+        info_merged.update(info_extra)
+        self.review_bot.comment_write(state='seen', result='failed', identical=True,
+                                      info_extra=info_extra, info_extra_identical=False,
+                                      project=PROJECT, message=COMMENT + '3')
+        comment, info_parsed = self.comments_filtered(self.bot)
+        self.assertTrue(comment['comment'].endswith(COMMENT + '3'))
+        self.assertEqual(info_parsed, info_merged)
+
+        # Final build (except comment replacement).
+        info_extra = {'build': '4'}
+        info_merged.update(info_extra)
+        self.review_bot.comment_write(state='seen', result='failed', identical=True,
+                                      info_extra=info_extra, info_extra_identical=True,
+                                      project=PROJECT, message=COMMENT + '4')
+        comment, info_parsed = self.comments_filtered(self.bot)
+        self.assertTrue(comment['comment'].endswith(COMMENT + '4'))
+        self.assertEqual(info_parsed, info_merged)
+
+        # Final build (except comment replacement).
+        info = {'state': 'done', 'result': 'passed'}
+        info_extra = {'build': '5'}
+        info_merged = info.copy()
+        info_merged.update(info_extra)
+        self.review_bot.comment_write(state='done', result='passed', identical=True,
+                                      info_extra=info_extra, info_extra_identical=True,
+                                      only_replace=True,
+                                      project=PROJECT, message=COMMENT + '5')
+        comment, info_parsed = self.comments_filtered(self.bot)
+        self.assertTrue(comment['comment'].endswith(COMMENT + '5'))
+        self.assertEqual(info_parsed, info_merged)
+
+        # Should never be more than one new comment.
+        self.assertEqual(len(self.api.get_comments(project_name=PROJECT)), comment_count + 1)
+
     def comments_filtered(self, bot):
         comments = self.api.get_comments(project_name=PROJECT)
         return self.api.comment_find(comments, bot)
