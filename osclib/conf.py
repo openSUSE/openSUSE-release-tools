3261aed8a10e788e1307931d210868a3de3882f5
Jimmy Berry <jberry@suse.com>
2018-02-06 02:09:59 -0600

    osclib/conf: leap: incorporate proven splitter settings.

--- osclib/conf.py
+++ osclib/conf.py
@@ -70,6 +70,10 @@ DEFAULT = {
         # check_source.py
         # review-team optionally added by leaper.py.
         'repo-checker': 'repo-checker',
+        # 16 hour staging window for follow-ups since lower throughput.
+        'splitter-staging-age-max': '57600',
+        # No special packages since they will pass through SLE first.
+        'splitter-special-packages': '',
         'pkglistgen-archs': 'x86_64',
         'pkglistgen-archs-ports': 'aarch64',
         'pkglistgen-locales-from': 'openSUSE.product',
