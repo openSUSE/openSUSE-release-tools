341396b4329c8f90d3bd878666a46d77cbead25b
Jimmy Berry <jberry@suse.com>
2019-03-05 08:40:47 -0600

    osclib/origin: config_resolve_create_family(): force origin_expanded to string.
    Drops the lxml string element reference which is cumbersome when printing.

--- osclib/origin.py
+++ osclib/origin.py
@@ -164,7 +164,7 @@ def config_resolve_create_workarounds(config, values_workaround, origins_skip):
 def config_resolve_create_family(apiurl, project, config, position, origin, values):
     projects = project_list_family_prior_pattern(apiurl, origin, project)
     for origin_expanded in reversed(projects):
-        config['origins'].insert(position, { origin_expanded: values })
+        config['origins'].insert(position, { str(origin_expanded): values })
 
 def config_resolve_apply(config, values_apply, key=None, workaround=False, until=None):
     for origin, values in config_origin_generator(config['origins']):
