99299a1010c6559d4fde4e7c5fd40fee490428cd
Stephan Kulow <coolo@suse.de>
2019-03-28 11:32:49 +0100

    Move some more pipelines from citest to official repo

--- gocd/generate.sh
+++ gocd/generate.sh
@@ -1,3 +1,15 @@
+#! /bin/sh
+
+set -e
+
+if ! test -f yaml-config-plugin-0.9.0.jar; then
+  wget https://github.com/tomzo/gocd-yaml-config-plugin/releases/download/0.9.0/yaml-config-plugin-0.9.0.jar
+fi
+
 for file in *.erb; do
   erb -T - $file > $(basename $file .erb)
+done
+
+for file in *.gocd.yaml; do
+  java -jar yaml-config-plugin-0.9.0.jar syntax $file
 done
