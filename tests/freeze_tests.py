1f17951eba8361763fcbd146e48419da01718069
Stephan Kulow <coolo@suse.de>
2019-04-23 21:25:16 +0200

    Port freeze tests to vcr

--- tests/freeze_tests.py
+++ tests/freeze_tests.py
@@ -4,21 +4,16 @@ import difflib
 import subprocess
 import tempfile
 
-from . import obs
-
 from osclib.conf import Config
 from osclib.freeze_command import FreezeCommand
 from osclib.stagingapi import StagingAPI
 
+import vcr
+from . import vcrhelpers
+
+my_vcr = vcr.VCR(cassette_library_dir='tests/fixtures/vcr/freeze')
 
 class TestFreeze(unittest.TestCase):
-    def setUp(self):
-        """
-        Initialize the configuration
-        """
-        self.obs = obs.OBS()
-        Config(obs.APIURL, 'openSUSE:Factory')
-        self.api = StagingAPI(obs.APIURL, 'openSUSE:Factory')
 
     def _get_fixture_path(self, filename):
         """
@@ -32,9 +27,11 @@ class TestFreeze(unittest.TestCase):
         """
         return os.path.join(os.getcwd(), 'tests/fixtures')
 
+    @my_vcr.use_cassette
     def test_bootstrap_copy(self):
+        wf = vcrhelpers.StagingWorkflow()
 
-        fc = FreezeCommand(self.api)
+        fc = FreezeCommand(wf.api)
 
         fp = self._get_fixture_path('staging-meta-for-bootstrap-copy.xml')
         fixture = subprocess.check_output('/usr/bin/xmllint --format %s' % fp, shell=True)
