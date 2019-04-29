4382f0eb300c929a1e2354be8dde01e689692e90
Stephan Kulow <coolo@suse.de>
2019-04-29 16:58:22 +0200

    Add another test actually unselecting

--- tests/unselect_tests.py
+++ tests/unselect_tests.py
@@ -1,7 +1,9 @@
 import unittest
 from osclib.conf import Config
 from osclib.stagingapi import StagingAPI
+from osclib.select_command import SelectCommand
 from osclib.unselect_command import UnselectCommand
+from osclib.core import package_list_without_links
 
 import vcr
 from . import vcrhelpers
@@ -17,3 +19,18 @@ class TestUnselect(unittest.TestCase):
         UnselectCommand.cleanup_days = 1
         obsolete = wf.api.project_status_requests('obsolete', UnselectCommand.filter_obsolete)
         self.assertSequenceEqual([], obsolete)
+
+    @my_vcr.use_cassette
+    def test_free_staging(self):
+        wf = vcrhelpers.StagingWorkflow()
+        wf.setup_rings()
+
+        staging_a = wf.create_staging('A', freeze=True)
+        winerq = wf.create_submit_request('devel:wine', 'wine')
+        self.assertEqual(True, SelectCommand(wf.api, staging_a.name).perform(['wine']))
+        self.assertEqual(['wine'], package_list_without_links(wf.apiurl, staging_a.name))
+
+        uc = UnselectCommand(wf.api)
+        self.assertIsNone(uc.perform(['wine']))
+
+        self.assertEqual([], package_list_without_links(wf.apiurl, staging_a.name))
