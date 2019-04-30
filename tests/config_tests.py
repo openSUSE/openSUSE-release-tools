4edfe9a45cbde430db91d944c9720a50ea44b7e5
Stephan Kulow <coolo@suse.de>
2019-04-30 12:06:35 +0200

    More import changes (to be squashed)

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -6,36 +6,34 @@ from osclib.core import attribute_value_save
 from osclib.memoize import memoize_session_reset
 from osclib.stagingapi import StagingAPI
 
-from obs import APIURL
-from obs import PROJECT
-from obs import OBS
+from . import obs
 
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
-        self.obs = OBS()
+        self.obs = obs.OBS()
         self.load_config()
-        self.api = StagingAPI(APIURL, PROJECT)
+        self.api = StagingAPI(obs.APIURL, obs.PROJECT)
 
-    def load_config(self, project=PROJECT):
-        self.config = Config(APIURL, project)
+    def load_config(self, project=obs.PROJECT):
+        self.config = Config(obs.APIURL, project)
 
     def test_basic(self):
-        self.assertEqual('openSUSE', conf.config[PROJECT]['lock-ns'])
+        self.assertEqual('openSUSE', conf.config[obs.PROJECT]['lock-ns'])
 
     def test_remote(self):
         # Initial config present in fixtures/oscrc and obs.py attribute default.
         # Local config fixture contains overridden-by-local and should win over
         # the remote config value.
-        self.assertEqual('local', conf.config[PROJECT]['overridden-by-local'])
-        self.assertEqual('remote-indeed', conf.config[PROJECT]['remote-only'])
+        self.assertEqual('local', conf.config[obs.PROJECT]['overridden-by-local'])
+        self.assertEqual('remote-indeed', conf.config[obs.PROJECT]['remote-only'])
 
         # Change remote value.
-        attribute_value_save(APIURL, PROJECT, 'Config', 'remote-only = new value\n')
+        attribute_value_save(obs.APIURL, obs.PROJECT, 'Config', 'remote-only = new value\n')
         self.load_config()
 
-        self.assertEqual('local', conf.config[PROJECT]['overridden-by-local'])
-        self.assertEqual('new value', conf.config[PROJECT]['remote-only'])
+        self.assertEqual('local', conf.config[obs.PROJECT]['overridden-by-local'])
+        self.assertEqual('new value', conf.config[obs.PROJECT]['remote-only'])
 
     def test_remote_none(self):
         self.load_config('not_real_project')
@@ -63,16 +61,16 @@ class TestConfig(unittest.TestCase):
         # Ensure each pattern is match instead of catch-all pattern.
         patterns = set()
         for project in projects:
-            config = Config(APIURL, project)
+            config = Config(obs.APIURL, project)
             patterns.add(conf.config[project]['pattern'])
 
         self.assertEqual(len(patterns), len(DEFAULT))
 
     def test_get_memoize_reset(self):
         """Ensure memoize_session_reset() properly forces re-fetch of config."""
-        self.assertEqual('remote-indeed', Config.get(APIURL, PROJECT)['remote-only'])
+        self.assertEqual('remote-indeed', Config.get(obs.APIURL, obs.PROJECT)['remote-only'])
 
-        attribute_value_save(APIURL, PROJECT, 'Config', 'remote-only = new value\n')
+        attribute_value_save(obs.APIURL, obs.PROJECT, 'Config', 'remote-only = new value\n')
         memoize_session_reset()
 
-        self.assertEqual('new value', Config.get(APIURL, PROJECT)['remote-only'])
+        self.assertEqual('new value', Config.get(obs.APIURL, obs.PROJECT)['remote-only'])
