2189443994fa90b791e0e402d1909f479c0bdf7c
Jimmy Berry <jberry@suse.com>
2019-08-27 14:13:10 -0500

    osclib/git: remove unnecessary CacheManager import.

--- osclib/git.py
+++ osclib/git.py
@@ -1,10 +1,6 @@
 import os
 from os import path
-from osclib.cache_manager import CacheManager
 import subprocess
-
-# Git will not be happy if pruned, but not used enough to be worth excluding.
-CACHE_DIR = CacheManager.directory('git')
 
 def clone(url, directory):
     return_code = subprocess.call(['git', 'clone', url, directory])
