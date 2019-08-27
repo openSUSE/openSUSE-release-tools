37386e0c74ebd5d0d4001d1cb9f5b3f92d999e0a
Jimmy Berry <jberry@suse.com>
2019-08-27 14:15:52 -0500

    osclib/common: provide version_calcuate() and utilize by default.

--- osclib/common.py
+++ osclib/common.py
@@ -1,2 +1,14 @@
 NAME = 'openSUSE-release-tools'
-VERSION = '0.0.0-dev'
+
+def version_calculate():
+    from os import path
+    if path.exists('.git'):
+        from osclib.git import describe
+        try:
+            return describe()
+        except FileNotFoundError:
+            pass # Fall through to final return.
+
+    return '0.0.0-dev'
+
+VERSION = version_calculate()
