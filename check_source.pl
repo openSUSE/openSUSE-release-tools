75594ade61229c1ad80390a73f07f08f856ecd99
Dominique Leuenberger <dimstar@opensuse.org>
2020-12-11 11:55:57 +0100

    check_source: mode='manual' is permitted in _service files

--- check_source.pl
+++ check_source.pl
@@ -21,8 +21,8 @@ if (-f "$dir/_service") {
     my $service = XMLin("$dir/_service", ForceArray => ['service']);
     while( my ($name, $s) = each %{$service->{service}} ) {
         my $mode = $s->{mode} || '';
-        next if ($mode eq "localonly" || $mode eq "disabled" || $mode eq "buildtime");
-        print "Services are only allowed if they are mode='localonly', 'disabled' or 'buildtime'. Please change the mode of $name and use `osc service localrun/disabledrun`.\n";
+        next if ($mode eq "localonly" || $mode eq "disabled" || $mode eq "buildtime" || $mnode eq "manual" );
+        print "Services are only allowed if they are mode='localonly', 'disabled', 'manual' or 'buildtime'. Please change the mode of $name and use `osc service localrun/disabledrun`.\n";
         $ret = 1;
     }
     # move it away to have full service from source validator
