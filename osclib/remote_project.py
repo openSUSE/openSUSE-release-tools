83ab175b01394481ac2ecbdfd91fb9e0a9bafec5
Josef Reidinger <jreidinger@suse.cz>
2021-08-19 16:57:11 +0200

    add initial remote project class

---
+++ osclib/remote_project.py
@@ -0,0 +1,62 @@
+import logging
+
+from lxml import etree as ET
+
+import osc.core
+import osc.conf
+
+from urllib.error import HTTPError, URLError
+
+class ProjectNotFound(Exception):
+    """Raised when Project is not found on server"""
+    pass
+
+class RemoteProject(object):
+    """This class represents a project on build service side.
+
+    The class offers methods to query and modify the project.
+    The class methods can be used to find or create projects.
+
+    Not to be confused with the class Project in osc.core_, aimed to local checkout of project
+
+    .. _osc.core: https://github.com/openSUSE/osc/blob/master/osc/core.py
+
+    """
+    def __init__(self, name):
+        self.name = name
+        self.metadata = None
+
+    @classmethod
+    def find(cls, name):
+        """Raise ProjectNotFound if not found"""
+        metadata = ProjectMetadata.load(name)
+        res = cls(name)
+        res.metadata = metadata
+
+        return res
+
+class ProjectMetadata(object):
+    def __init__(self, linked_projects):
+        self.linked_projects = linked_projects
+
+    @classmethod
+    def parse(cls, content):
+        data = ET.parse(content)
+        linked_projects = []
+        for child in data.getroot():
+            if child.tag == 'link':
+                linked_projects.append(child.attrib['project'])
+
+        return cls(linked_projects)
+
+    @classmethod
+    def load(cls, project_name):
+        url = osc.core.make_meta_url('prj', project_name, osc.conf.config['apiurl'])
+        try:
+            return cls.parse(osc.core.http_GET(url))
+        except HTTPError as e:
+            if e.code == 404:
+                raise ProjectNotFound("Project %s not found" % (project_name))
+            else:
+                raise
+
--- rebuildability_check.py
+++ rebuildability_check.py
@@ -4,24 +4,27 @@ import argparse
 import logging
 
 import osc.core
+import osc.conf
 
-from osclib.conf import Config
+import osclib.remote_project
+from osclib.remote_project import RemoteProject
 
 class RebuildabilityChecker(object):
     def __init__(self, project_str):
         self.logger = logging.getLogger('RebuildibilityChecker')
-        self.project = Project.find(project_str) # apiurl should be read from osc.conf.config['apiurl'], osclib config class looks like has different goal?
+        self.project = RemoteProject.find(project_str) # apiurl should be read from osc.conf.config['apiurl'], osclib config class looks like has different goal?
 
-    def result(self)
+    def result(self):
         packages = self.project.get_packages(recursive = True) # package should include also project from which it comes. Recursive means include also inherited packages so reading meta and linked projects are needed
 
         rebuild_project = self.project.create_subproject("Rebuild") # how to handle if it exists? Clean it and use? What if we do not have permission to create subproject?
         testing_packages = [package.copy(rebuild_project) for package in packages]
-        while not all([p.builds.is_finished for p in testing_packages]) # builds is object for handling building and is_finished means that all builds are finished ( or disabled )
+        while not all([p.builds.is_finished for p in testing_packages]): # builds is object for handling building and is_finished means that all builds are finished ( or disabled )
           # write some progress about number of pass, failures, what is waiting, etc. using p.builds query ( ensure it is not memoized )
+          print("Working like a crazy monk")
 
         # TODO: delete rebuild project or keep it for inspection?
-        return all([not p.builds.any_failed for p in testing_packages]
+        return all([not p.builds.any_failed for p in testing_packages])
 
 
 if __name__ == '__main__':
