ca9f8b9c4c02c675b5ed0797cf278ec85ead5fd4
Imobach González Sosa <igonzalezsosa@suse.com>
2021-08-23 11:47:33 +0100

    Extend RemotePackage with more meta data

--- osclib/remote_package.py
+++ osclib/remote_package.py
@@ -10,11 +10,27 @@ class PackageNotFound(Exception):
     """ The package was not found in the build service """
     pass
 
+
+class PackageMetadata(object):
+    """ It holds package meta information
+
+    NOTE: to be extended with more elements. Otherwise, it could live within RemotePackage.
+    """
+    def __init__(self, releasename=None):
+        self.releasename = releasename
+
+    @classmethod
+    def from_xml_node(cls, node):
+        return PackageMetadata(
+            releasename=node.findtext('releasename')
+        )
+
 class RemotePackage(object):
     """ This class represents a package on the build service side """
-    def __init__(self, name, project=None):
+    def __init__(self, name, project=None, meta=PackageMetadata()):
         self.name = name
         self.project_name = project
+        self.meta = meta
 
     def copy(self, target_project_name, expand=False):
         apiurl = osc.conf.config['apiurl']
@@ -22,11 +38,15 @@ class RemotePackage(object):
         return RemotePackage(self.name, target_project_name)
 
     @classmethod
-    def from_xml(cls, content):
+    def from_xml(cls, xml_tree):
         """ Returns a project from an XML node (lxml.etree._ElementTree) """
-        data = ET.parse(content)
-        node = data.getroot()
-        return RemotePackage(node.get('name'), project=node.get('project'))
+        node = xml_tree.getroot()
+
+        return RemotePackage(
+            node.get('name'),
+            project=node.get('project'),
+            meta=PackageMetadata.from_xml_node(node)
+        )
 
     @classmethod
     def find(cls, project_name, package_name):
@@ -37,7 +57,8 @@ class RemotePackage(object):
         """
         url = osc.core.make_meta_url('pkg', (project_name, package_name), osc.conf.config['apiurl'])
         try:
-            return cls.from_xml(osc.core.http_GET(url))
+            xml_tree = ET.parse(osc.core.http_GET(url))
+            return cls.from_xml(xml_tree)
         except HTTPError as e:
             if e.code == 404:
                 raise PackageNotFound('Package %s not found' % (package_name))
