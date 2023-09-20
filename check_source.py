8e55eb3d0a9350c58dde253a356c987a6f678150
Fabian Vogt <fvogt@suse.de>
2023-09-20 08:56:16 +0200

    check_source.py: Add review-team review on meta packages
    All submissions must be reviewed before they can be accepted.

--- check_source.py
+++ check_source.py
@@ -137,7 +137,11 @@ class CheckSource(ReviewBot.ReviewBot):
 
         kind = package_kind(self.apiurl, target_project, target_package)
         if kind == 'meta':
-            self.review_messages['accepted'] = 'Skipping all checks for meta packages'
+            self.review_messages['accepted'] = 'Skipping most checks for meta packages'
+            if not self.skip_add_reviews and self.add_review_team and self.review_team is not None:
+                if not (self.allow_valid_source_origin and source_project in self.valid_source_origins):
+                    self.add_review(self.request, by_group=self.review_team, msg='Please review sources')
+
             return True
         elif (kind is not None and kind != 'source'):
             self.review_messages['declined'] = 'May not modify a non-source package of type {}'.format(kind)
