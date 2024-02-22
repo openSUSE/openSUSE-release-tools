db4bd757d4b9ac8226cfe9de3000c07ba917844b
Richard Brown <rbrown@suse.com>
2024-02-22 14:41:40 +0100

    Introduce new openSUSE|SUSE.Legal group and move pipelines into them

--- gocd/generate.sh
+++ gocd/generate.sh
@@ -14,7 +14,7 @@ done
 
 grep group: *.yaml | cut -d: -f3 | sort -u | while read group; do
   case $group in
-    BCI|Factory|Leap|Admin|MicroOS|Monitors|openSUSE.Checkers|SLE15.Stagings|SLE15.Target|SLE.Checkers|ALP.Stagings|ALP.Target|ALP.Checkers)
+    BCI|Factory|Leap|Admin|MicroOS|Monitors|openSUSE.Checkers|SLE15.Stagings|SLE15.Target|SLE.Checkers|ALP.Stagings|ALP.Target|ALP.Checkers|openSUSE.Legal|SUSE.Legal)
     ;;
   *)
     echo "Do not create new groups without being admin and knowing the consequences - found $group"
