2850de1852c6ecf3bfe48bb7902323911fe85910
Giacomo Leidi <giacomo.leidi@suse.com>
2024-09-16 15:26:25 +0200

    gocd: Add SUSE:SLFO:1.1 pipelines.

--- gocd/generate.sh
+++ gocd/generate.sh
@@ -14,7 +14,7 @@ done
 
 grep group: *.yaml | cut -d: -f3 | sort -u | while read group; do
   case $group in
-    BCI|Factory|Leap|Admin|MicroOS|Monitors|openSUSE.Checkers|SLE15.Stagings|SLE15.Target|SLE.Checkers|ALP.Stagings|ALP.Checkers|SLFO.Stagings|SLFO.Target|SLFO.Checkers|openSUSE.Legal|SUSE.Legal|SLES)
+    BCI|Factory|Leap|Admin|MicroOS|Monitors|openSUSE.Checkers|SLE15.Stagings|SLE15.Target|SLE.Checkers|ALP.Stagings|ALP.Checkers|SLFO.Stagings|SLFO.Target|SLFO.Checkers|SLFO.1.1.Stagings|SLFO.1.1.Target|openSUSE.Legal|SUSE.Legal|SLES)
     ;;
   *)
     echo "Do not create new groups without being admin and knowing the consequences - found $group"
