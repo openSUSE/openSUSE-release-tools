17a3ae38ab01fef49ab44f6f3006bf661d92ee34
Yu Daike <yu.daike@suse.com>
2025-02-04 12:07:03 +0100

    feat: add stub git implementation

--- vcs/__init__.py
+++ vcs/__init__.py
@@ -1,2 +1,3 @@
 from vcs.osc import OSC
+from vcs.git import Git
 
---
+++ vcs/git.py
@@ -0,0 +1,20 @@
+import vcs.base
+
+class Git(vcs.base.VCSBase):
+    """VCS interface implementation for Git"""
+
+    def __init__(self):
+        # XXX stub
+        pass
+
+    @property
+    def name(self) -> str:
+        return "git"
+
+    def get_path(self, *args):
+        # XXX stub
+        raise NotImplementedError
+
+    def get_request(self, request_id, with_full_history=False):
+        # XXX stub
+        raise NotImplementedError
