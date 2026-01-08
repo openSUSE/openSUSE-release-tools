ff6fe1a7a30a20634a688ff81ec37ef8fa29cc57
Giacomo Leidi <giacomo.leidi@suse.com>
2026-01-08 12:57:32 +0100

    check_bugowner: Add Git workflow compatible bugowner checker.

--- check_bugowner.py
+++ check_bugowner.py
@@ -2,9 +2,15 @@
 
 # SPDX-License-Identifier: MIT
 
+import json
+import os
+import shutil
 import sys
 import re
+from pathlib import Path
+from typing import Set
 
+from git.config import GitConfigParser
 from urllib.error import HTTPError
 
 import osc.conf
@@ -12,6 +18,7 @@ import osc.core
 import ReviewBot
 
 http_GET = osc.core.http_GET
+MAINTAINERSHIP_FILE = "_maintainership.json"
 
 
 class CheckerBugowner(ReviewBot.ReviewBot):
@@ -21,7 +28,7 @@ class CheckerBugowner(ReviewBot.ReviewBot):
         self.request_default_return = True
         self.override_allow = False
 
-    def check_source_submission(self, src_project, src_package, src_rev, target_project, target_package):
+    def _obs_check_source_submission(self, src_project, src_package, src_rev, target_project, target_package):
         self.logger.info("%s/%s@%s -> %s/%s" % (src_project,
                                                 src_package, src_rev, target_project, target_package))
         if src_package.startswith('patchinfo'):
@@ -70,6 +77,149 @@ class CheckerBugowner(ReviewBot.ReviewBot):
     def exists_in(self, project, package):
         url = osc.core.makeurl(self.apiurl, ['source', project, package])
         return self.existing_url(url)
+
+    @staticmethod
+    def get_request_from_src_rev(requests, src_rev):
+        for request in requests:
+            if request.actions[0].src_rev == src_rev:
+                return request
+
+        return None
+
+    def _gitea_checkout(self, project: str, package: str, revision: str):
+        local_dir = Path(
+            os.path.expanduser(
+                f"~/.cache/bugowner_checker_git/{project}_{package}_{revision}"
+            )
+        )
+        if local_dir.is_dir():
+            self.logger.warning(f"directory {local_dir} already exists, removing it.")
+            shutil.rmtree(local_dir, ignore_errors=True)
+
+        local_dir.mkdir(parents=True)
+
+        self.scm.checkout_package(
+            target_project=project,
+            target_package=package,
+            pathname=local_dir,
+            revision=revision,
+        )
+
+        return Path(local_dir, package)
+
+    def _diff_submodules(self, head_gitmodules: Path, base_gitmodules: Path):
+        head_config = GitConfigParser(file_or_files=head_gitmodules, read_only=True)
+        base_config = GitConfigParser(file_or_files=base_gitmodules, read_only=True)
+
+        head_sections = head_config.sections()
+        base_sections = base_config.sections()
+
+        self.logger.debug(f"HEAD submodule sections: {head_sections}")
+        self.logger.debug(f"base submodule sections: {base_sections}")
+
+        head_submodules = {
+            section for section in head_sections if section.startswith("submodule ")
+        }
+        base_submodules = {
+            section for section in base_sections if section.startswith("submodule ")
+        }
+
+        new_submodule_sections = head_submodules - base_submodules
+
+        new_submodules = set()
+        if new_submodule_sections:
+            for section in new_submodule_sections:
+                try:
+                    # Extract name from 'submodule "name"'
+                    name = section.split('"')[1]
+                    new_submodules.add(name)
+                    self.logger.info(f"Found new submodule: {name}")
+                except IndexError:
+                    self.logger.error(f"Could not parse submodule section: {section}")
+        else:
+            self.logger.info("No new submodules found.")
+
+        return new_submodules
+
+    def _load_maintainership_data(self, file: Path) -> Set[str]:
+        with open(file) as f:
+            # Assuming it's a JSON dictionary
+            data = json.load(f)
+
+            if not isinstance(data, dict):
+                raise ValueError(f"Maintainership file '{MAINTAINERSHIP_FILE}' must contain a JSON dict, but {type(data)} was found.")
+
+            # Convert list of package names to a set for fast lookups
+            return set(data.keys())
+
+    def _gitea_validate(
+        self,
+        head_project: str,
+        head_package: str,
+        head_revision: str,
+        base_project: str,
+        base_package: str,
+        base_revision: str,
+    ) -> bool:
+        head_dir = self._gitea_checkout(head_project, head_package, head_revision)
+        base_dir = self._gitea_checkout(base_project, base_package, base_revision)
+
+        new_submodules = self._diff_submodules(
+            Path(head_dir, ".gitmodules"), Path(base_dir, ".gitmodules")
+        )
+
+        maintained = self._load_maintainership_data(Path(head_dir, MAINTAINERSHIP_FILE))
+
+        orphan_packages = set()
+        for package in new_submodules:
+            if package not in maintained:
+                orphan_packages.add(package)
+
+        return orphan_packages
+
+    def _gitea_check_source_submission(
+        self,
+        head_project: str,
+        head_package: str,
+        head_revision: str,
+        base_project: str,
+        base_package: str,
+    ) -> bool:
+
+        request = self.get_request_from_src_rev(self.requests, head_revision)
+        if not request:
+            self.logger.warning(f"Request with HEAD {head_revision} not found!")
+            return None
+        base_revision = request.actions[0].tgt_rev
+
+        self.logger.debug(
+            f"{head_project}/{head_package}@{head_revision} -> {base_project}/{base_package}@{base_revision}"
+        )
+
+        orphans = self._gitea_validate(
+            head_project,
+            head_package,
+            head_revision,
+            base_project,
+            base_package,
+            base_revision,
+        )
+
+        is_valid = len(orphans) == 0
+
+        if is_valid:
+            self.review_messages["accepted"] = "The change does not introduce orphan packages."
+        else:
+            self.review_messages["declined"] = f"Missing maintainership information for {', '.join(orphans)}." + \
+                                               f" Please edit {MAINTAINERSHIP_FILE} and resubmit."
+
+        return is_valid
+
+    def check_source_submission(self, src_project, src_package, src_rev, target_project, target_package):
+        if self.platform.name == "GITEA":
+            return self._gitea_check_source_submission(src_project, src_package, src_rev, target_project, target_package)
+        else:
+            return self._obs_check_source_submission(src_project, src_package, src_rev, target_project, target_package)
 
 
 class CommandLineInterface(ReviewBot.CommandLineInterface):
